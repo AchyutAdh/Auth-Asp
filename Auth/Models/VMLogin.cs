0db75959cfac0561db43e7253f04341fbdc18b6c
uSer <uSer@DESKTOP-Q5RS1JR>
2023-04-20 09:16:39 +0545

    Add project files.

---
+++ Auth/Models/VMLogin.cs
@@ -0,0 +1,12 @@
+﻿namespace Auth.Models
+{
+    public class VMLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public bool KeepLoggendIn { get; set; }
+
+
+    }
+}
